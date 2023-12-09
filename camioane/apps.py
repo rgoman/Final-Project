9c566d29df85614995748ae5c1d894cd1f797540
rgoman <razvan.goman@gmail.com>
2023-12-09 10:59:24 +0200

    first commit

---
+++ camioane/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ListaCamioaneConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'camioane'
---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+# from student import views
+
+urlpatterns = [
+    # path('create_student/', views.StudentCreateView.as_view(), name = 'create-student'),
+    # path('list_of_students/', views.StudentListView.as_view(), name = 'list-of-students'),
+    # path('update_student/<int:pk>/', views.StudentUpdateView.as_view(), name = 'update-student'),
+    # path('delete_student/<int:pk>/', views.StudentDeleteView.as_view(), name = 'delete-student'),
+    # path('detail_student/<int:pk>/', views.StudentDetailView.as_view(), name = 'detail-student'),
+]
